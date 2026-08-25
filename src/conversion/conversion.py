6b506c0b456deb3446917fbe6bea3a352eb1ad8c
Anita <ammurcia54@ucatolica.edu.co>
2026-08-24 20:05:37 -0500

    Funcion de pies a metros

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -71,7 +71,8 @@ class Conversion:
         Ejemplo:
             pies_a_metros(3.28084) -> 1.0
         """
-        pass
+        resultado = pies * 0.3048
+        return resultado
     
     def decimal_a_binario(self, decimal):
         """
