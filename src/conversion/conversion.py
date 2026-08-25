41e997482f40527aed60c3bd269c3aea9282a8c3
Anita <ammurcia54@ucatolica.edu.co>
2026-08-24 21:41:35 -0500

    Funcion de morse a texto

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -220,4 +220,24 @@ class Conversion:
             morse_a_texto("... --- ...") -> "SOS"
             morse_a_texto(".... . .-.. .-.. ---") -> "HELLO"
         """
-        pass
+        morse_dict = {
+            'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 
+            'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 
+            'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 
+            'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 
+            'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+            'Y': '-.--', 'Z': '--..',
+            '0': '-----', '1': '.----', '2': '..---', 
+            '3': '...--', '4': '....-', '5': '.....', 
+            '6': '-....', '7': '--...', 
+            '8': '---..', '9': '----.'
+        }
+        texto = []
+        for code in morse.split(" "):
+            for char, morse_code in morse_dict.items():
+                if code == morse_code:
+                    texto.append(char)
+                    break
+            else:
+                raise ValueError(f"Codigo Morse no soportado: {code}")
+        return "".join(texto)
