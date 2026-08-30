d66ede84c3fb0657be297711d48f6cb3b3d3a520
Anita <ammurcia54@ucatolica.edu.co>
2026-08-30 17:17:32 -0500

    volumen cubo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -154,7 +154,7 @@ class Geometria:
         Returns:
             float: Perímetro del pentágono regular
         """
-        return 5 * lado 
+        return 5 * lado
     
     def area_hexagono_regular(self, lado, apotema):
         """
@@ -167,8 +167,9 @@ class Geometria:
         Returns:
             float: Área del hexágono regular
         """
-        pass
-    
+        p = 6 * lado
+        return (p * apotema) / 2 
+     
     def perimetro_hexagono_regular(self, lado):
         """
         Calcula el perímetro de un hexágono regular.
@@ -179,7 +180,7 @@ class Geometria:
         Returns:
             float: Perímetro del hexágono regular
         """
-        pass
+        return 6 * lado 
     
     def volumen_cubo(self, lado):
         """
@@ -191,7 +192,7 @@ class Geometria:
         Returns:
             float: Volumen del cubo
         """
-        pass
+        return lado ** 3 
     
     def area_superficie_cubo(self, lado):
         """
