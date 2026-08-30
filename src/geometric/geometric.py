71ee612fe01680fbe491e910e315a1fe63d3fc88
Anita <ammurcia54@ucatolica.edu.co>
2026-08-30 17:07:20 -0500

    area rombo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -129,7 +129,7 @@ class Geometria:
         Returns:
             float: Área del rombo
         """
-        pass
+        return (diagonal_mayor * diagonal_menor) /2 
     
     def area_pentagono_regular(self, lado, apotema):
         """
