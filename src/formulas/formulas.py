44dfe750b10d8c46c4d6ed2e0e3d8107e52fb6c3
Anita <ammurcia54@ucatolica.edu.co>
2026-08-30 16:20:56 -0500

    formula cuadratica raices

--- src/formulas/formulas.py
+++ src/formulas/formulas.py
@@ -227,7 +227,14 @@ class Formulas:
         Ejemplo:
             raices_cuadraticas(1, -3, 2) -> (2.0, 1.0)
         """
-        pass
+        if a == 0:
+            raise ValueError("El coeficiente a no puede ser 0")
+        discriminante = b**2 - 4*a*c
+        if discriminante < 0:
+            raise ValueError("No hay raices reales")
+        raiz1 = (- b + discriminante ** (1/2)) / (2 * a)
+        raiz2 = (- b - discriminante ** (1/2)) / (2 * a)
+        return raiz1, raiz2
 
     def imc(self, peso, altura):
         """
